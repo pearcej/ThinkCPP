8b7c03d66d4c27bbde03bcd5f32e7802ecc763e3
Andrew Scholer <ascholer@chemeketa.edu>
2024-10-08 08:51:09 -0700

    ch1 more cleaning

--- kebab.sh
+++ kebab.sh
@@ -3,9 +3,23 @@
 # done
 
 for file in *; do
-  mv "$file" -- "$(echo $file | sed -r 's/([A-Z])/-\L\1/g')"
+  mv -f -- "$file" "$(echo $file | sed 's/_/-/')"
+  # echo "$(echo $file | sed 's/_/-/')"
 done
+# echo "------------------------------------------"
 
 for file in *; do
-  mv -- "$file" "$(echo $file | sed 's/^-//')"
+  mv -f -- "$file" "$(echo $file | sed -r 's/([A-Z])/-\L\1/g')"
+  # echo $file | sed -r 's/([A-Z])/_\L\1/g'
 done
+
+# echo "------------------------------------------"
+for file in *; do
+  mv -f -- "$file" "$(echo $file | sed 's/^-//')"
+  #echo $file | sed 's/^_//'
+done
+# echo "------------------------------------------"
+# for file in *; do
+#   #mv -f -- "$file" "$(echo $file | sed 's/^-//')"
+#   echo "_${file}"
+# done
