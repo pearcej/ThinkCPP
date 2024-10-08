18cb2306402c896a85ddc4ca1786cf6f432a02a2
Andrew Scholer <ascholer@chemeketa.edu>
2024-10-08 09:59:35 -0700

    file name cleanup

--- kebab.sh
+++ kebab.sh
@@ -1,9 +1,6 @@
-# for file in *; do
-#  echo "$(echo $file | sed -r 's/([A-Z])/-\L\1/g')"
-# done
 
 for file in *; do
-  mv -f -- "$file" "$(echo $file | sed 's/_/-/')"
+  mv -f -- "$file" "$(echo $file | sed 's/_/-/g')"
   # echo "$(echo $file | sed 's/_/-/')"
 done
 # echo "------------------------------------------"
@@ -13,13 +10,8 @@ for file in *; do
   # echo $file | sed -r 's/([A-Z])/_\L\1/g'
 done
 
-# echo "------------------------------------------"
+# # echo "------------------------------------------"
 for file in *; do
   mv -f -- "$file" "$(echo $file | sed 's/^-//')"
   #echo $file | sed 's/^_//'
 done
-# echo "------------------------------------------"
-# for file in *; do
-#   #mv -f -- "$file" "$(echo $file | sed 's/^-//')"
-#   echo "_${file}"
-# done
